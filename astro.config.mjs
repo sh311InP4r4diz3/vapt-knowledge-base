7dc8eac038cdbade4128051e2c2cce3ea3d3356a
Saikiran Boiny <103438798+sh311InP4r4diz3@users.noreply.github.com>
2026-06-14 15:18:22 +0530

    Fix base path in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sh311InP4r4diz3.github.io',
-  base: '/vapt-knowledge-base',
+  base: '/vapt-knowledge-base/',
   markdown: {
     shikiConfig: {
       theme: 'github-dark-dimmed',
