a0c07c3f517962fd14aec748d468f728e4c3da97
Saikiran Boiny <103438798+sh311InP4r4diz3@users.noreply.github.com>
2026-06-14 14:10:56 +0530

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://sh3llInP4r4diz3.github.io',
+  site: 'https://sh311InP4r4diz3.github.io',
   base: '/vapt-knowledge-base',
   markdown: {
     shikiConfig: {
